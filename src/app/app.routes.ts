ee5a10a9eed37d87837cde11d6fc07d355a859f6
jose angel <jose.albelt1817@gmail.com>
2025-03-07 10:32:40 -0500

    Router

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    {
+        path:'dashboard',
+        loadComponent:() => import('./gifs/pages/dasboard-page/dasboard-page.component')
+    },
+    {
+        path:'**',
+        redirectTo:'dashboard'
+    }
+];
---
+++ src/app/gifs/pages/dasboard-page/dasboard-page.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dasboard-page',
+  imports: [],
+  templateUrl: './dasboard-page.component.html',
+})
+export default class DasboardPageComponent { }
