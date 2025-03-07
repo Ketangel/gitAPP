48f28c6f8985f816569deda08fd4da8903e2a1a0
jose angel <jose.albelt1817@gmail.com>
2025-03-07 14:22:31 -0500

    router links desde componentes hijos

--- src/app/gifs/components/gits-side-menu/gits-side-menu-options/gits-side-menu-options.component.ts
+++ src/app/gifs/components/gits-side-menu/gits-side-menu-options/gits-side-menu-options.component.ts
@@ -1,9 +1,29 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { MenuOption } from '../../../interfaces/menu-option.interfaces';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-gits-side-menu-options',
-  imports: [],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './gits-side-menu-options.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class GitsSideMenuOptionsComponent { }
+export class GitsSideMenuOptionsComponent { 
+
+
+  menuOptions: MenuOption[] = [
+    {
+      label: 'Trending',
+      subLabel: 'Gifs Populares',
+      router: '/dashboard/treading',
+      icon: 'fa-solid fa-chart-line'
+    },
+    {
+      label: 'Search',
+      subLabel: 'Buscar Gifs',
+      router: '/dashboard/search',
+      icon: 'fa-solid fa-magnifying-glass'
+    }
+  ]
+
+}
